5c03a8cb3ff272d6d15dafdee79e19893c10d247
Rito <116003382+vinhnamhai321@users.noreply.github.com>
2023-03-05 16:27:20 +0700

    Update Assignment 1.cpp

--- 22125096_W05_03/Assignment 1/Assignment 1.cpp
+++ 22125096_W05_03/Assignment 1/Assignment 1.cpp
@@ -4,14 +4,7 @@ int main()
 {
 	cout << "Hello Tuan Minh\n";
 	cout<<"Doan Cong Thanh\n";
-<<<<<<< Updated upstream
-	cout<<"Conflict'n";
-=======
-<<<<<<< HEAD
 	cout << "Huynh Tuan Minh";
-=======
 	cout<<"Conflict'n";
->>>>>>> e62d84b5906a28097969baf2d6032fd3a4fef641
->>>>>>> Stashed changes
 	return 0;
 }
